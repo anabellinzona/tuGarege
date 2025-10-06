da4eb6b147c14e37fb627cc6118597535c04c8fc
Ana Bellinzona <anabellinzona@gmail.com>
2025-10-06 12:43:35 -0300

    create the project

---
+++ src/main/java/com/miGarage/sistema_ventas_vehiculos/SistemaVentaVehiculosApplication.java
@@ -0,0 +1,13 @@
+package com.miGarage.sistema_ventas_vehiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaVentaVehiculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaVentaVehiculosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miGarage/sistema_ventas_vehiculos/SistemaVentaVehiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miGarage.sistema_ventas_vehiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaVentaVehiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

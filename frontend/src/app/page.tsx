bd3484b6b26e8759516624ab53336c3778f7b979
Ana Bellinzona <anabellinzona@gmail.com>
2025-10-08 19:22:53 -0300

    I add the info cards

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,11 @@
 import SearchSection from "../components/searchSection/searchSection";
+import InfoCard from "../components/infoCards/infoCards";
 
 export default function Home() {
   return (
       <main>
         <SearchSection />
+          <InfoCard />
       </main>
   );
 }

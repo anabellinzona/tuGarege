9515bfa07b7448186fea6b9456095fe972547839
Ana Bellinzona <anabellinzona@gmail.com>
2025-11-22 10:18:44 -0300

    fix the page´s imports

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,11 @@
 "use client"; // si estás en app router
 
-import SearchSection from "../components/searchSection/searchSection";
-import InfoCard from "../components/infoCards/infoCards";
-import FeatureCards from "../components/featuredCards/featuredCards";
+import SearchSection from "@/components/searchSection/searchSection"
+import InfoCard from "@/components/infoCards/infoCards";
+import FeatureCards from "@/components/featuredCards/featuredCards";
 import RecentsSection from "@/components/recentsSection/recentsSection";
 
 export default function Home() {
-    console.log('API URL:', process.env.NEXT_PUBLIC_API_URL);
   return (
       <main>
           <SearchSection />

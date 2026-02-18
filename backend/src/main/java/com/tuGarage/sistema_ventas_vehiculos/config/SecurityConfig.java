e443cd3046d51bf1e127066a10401c1dac9157c7
Ana Bellinzona <anabellinzona@gmail.com>
2026-02-18 12:05:56 -0300

    add the vercel addres to use the back in vercel

--- backend/src/main/java/com/tuGarage/sistema_ventas_vehiculos/config/SecurityConfig.java
+++ backend/src/main/java/com/tuGarage/sistema_ventas_vehiculos/config/SecurityConfig.java
@@ -42,7 +42,7 @@ public class SecurityConfig {
 
         // Frontend en producción
         config.setAllowedOrigins(List.of(
-                "https://tugarege.vercel.app",     // corregido
+                "https://tu_garege.vercel.app",     // corregido
                 "https://tugarege.onrender.com",   // agrega tu backend de Render
                 "http://localhost:3000"
         ));
